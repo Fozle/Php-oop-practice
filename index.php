9e7dd7479eb0a7c8a3bc31c9bda01d013247b7c5
Fozle <fozlearafat48@gmail.com>
2025-03-10 23:09:15 +0200

    update

--- index.php
+++ index.php
@@ -2,18 +2,29 @@
 
 class student{
     public $name;
-    function set_Name($str){
-        $this->name=$str;
-    }
-    function out_put(){
-       return $this->name;
-    }
+  function __construct($str){
+   $this->name = $str;
+    
 }
 
-$obj=new student();
-$obj1=new student();
-$obj->set_Name("Fozle Arafat 1");
-echo $obj->out_put();
-$obj1->set_Name("IOT STUdent");
-echo "<br/>". $obj1->out_put();
-?>
+
+}
+
+class classx extends student{
+   #method
+   function getName(){
+      return $this->name;
+   }
+}
+
+class classx1 extends student{
+   function getName(){
+      return $this->name;
+   }
+}
+
+$obj = new classx("Fozle Arafat");
+$obj1 = new classx1("Chowdhury");
+
+echo "classx ". $obj->getName();
+echo "<br/> Classx1 ". $obj1->getName();
